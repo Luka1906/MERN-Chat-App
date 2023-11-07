f564e112beaaa1b128e517b4e1eaff808e7f0b43
Luka Matovic <lukamatovic1994@gmail.com>
2023-11-07 04:45:49 -0600

    Changing environmental variables

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ const server = app.listen(port, () => console.log("Server is running"));
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   },
 });
